1e957189554a84560d55772a3286290dea787231
Simon Perryman <sperryman15@gmail.com>
2019-04-21 11:51:05 +0100

    Fixed a bug as I did not have two parameters in the callback function of a job (calculate_mood)

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 from environment_variables import add_environment_variables
 from misc import save_pickle, load_pickle
+import nltk.tokenize
 def test():
   add_environment_variables()
   print(os.environ['PICKLE_DIRECTORY'])
