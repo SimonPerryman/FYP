6d24139c1c20579d951abd2837cb612ff3b7ef5d
Simon Perryman <sperryman15@gmail.com>
2019-04-26 11:57:22 +0100

    Fixed some bugs

--- test.py
+++ test.py
@@ -1,50 +1,23 @@
 import os
-import spacy
+from nltk.corpus import movie_reviews
 os.environ['APPLICATION_PATH'] = r"C:/dev/projects/university/FYP/"
-from nlp_techniques import lemmatize_sentence
-nlp = spacy.load("en_core_web_lg")
-
-def check_for_crew(message):
-  """Searches for crew members in the message using NER and then checks
-  for keywords which a crew member's name would be expected to be followed by.
-  @param {String} message
-  @returns {List} crew names"""
-  crew = set()
-  crew_present = False
-  doc = nlp(u'{}'.format(message))
-  docLower = nlp(u'{}'.format(message.lower()))
-  for ent in doc.ents:
-    if ent.label_ == "PERSON":
-      crew.add(ent.text)
-    
-  for token in docLower:
-    if token.text == "with" or token.text == "starring":
-      message = message[token.idx:].title()
-      crew_present = True
-    
-    if token.text == "by":
-      for anc in token.ancestors:
-        if anc.text == "directed" or anc.text == "written":
-          message = message[token.idx:].title()
-          crew_present = True
-  print(message)
-  # if crew_present:
-  #   doc = nlp(message)
-  #   for child in doc[0].children:
-  #     if child.pos_ == "NOUN" or child.pos_ == "PROPN":
-
-
-  return list(crew)
+os.environ['PICKLE_DIRECTORY'] = r"C:/dev/projects/university/FYP/pickle_files"
+from misc import load_pickle
 
 def test():
-  check_for_crew("Suggest a film directed by Tom Cruise and Bruce Willis")
-  check_for_crew("Suggest a film starring Tom Cruise and Bruce Willis")
-  # doc = nlp(u"I really enjoy films about SpiderMan and Superman")
-
-  # for ent in doc.ents:
-  #     print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
-  return True
+    film_reviews = load_pickle('movie_reviews.pkl')
+    reviews = [((list(movie_reviews.words(fileid))), category)
+        for category in movie_reviews.categories()
+        for fileid in movie_reviews.fileids(category)]
+    min_len = 1000
+    max_len = 0
+    for review in reviews:
+        len_review = len(review[0])
+        if len_review < min_len:
+            min_len = len_review
+        if len_review > max_len:
+            max_len = len_review
+    return True
 
 if __name__ == '__main__':
-  test()
+    test()
